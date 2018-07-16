1c65a8874c3e68990df3c5d1cd6f57ba07e4ee79
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-16 18:29:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -16,3 +16,15 @@ end
 
 
 def countdown_with_sleep(number)
+  
+  number = 10
+ 
+  until number == 0
+    puts "#{number} SECOND(S)!"
+    sleep
+    number -= 1
+  end
+
+  return "HAPPY NEW YEAR!"
+
+end
